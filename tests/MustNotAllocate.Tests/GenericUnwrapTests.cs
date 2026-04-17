e9343416cb77b21cf01befbcbeac30b52aee6fd2
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2026-04-17 08:41:02 +0200

    test: add generic OriginalDefinition unwrap tests

---
+++ tests/MustNotAllocate.Tests/GenericUnwrapTests.cs
@@ -0,0 +1,52 @@
+using System.Threading.Tasks;
+using CallgraphClosure.Core;
+using Xunit;
+
+namespace MustNotAllocate.Tests;
+
+public class GenericUnwrapTests
+{
+    [Fact]
+    public async Task AnnotatedGenericCallee_ConstructedForm_DoesNotFireBoundary()
+    {
+        var source = """
+            using MustNotAllocate;
+
+            class C
+            {
+                [MustNotAllocate]
+                void Caller() { Callee<int>(); }
+
+                [MustNotAllocate]
+                void Callee<T>() { }
+            }
+            """;
+
+        await CSharpAnalyzerVerifier<MustNotAllocateAnalyzer>
+            .VerifyAnalyzerAsync(source);
+    }
+
+    [Fact]
+    public async Task UnannotatedGenericCallee_ConstructedForm_FiresOnceAsCGC001()
+    {
+        var source = """
+            using MustNotAllocate;
+
+            class C
+            {
+                [MustNotAllocate]
+                void Caller() { Callee<int>(); }
+
+                void Callee<T>() { }
+            }
+            """;
+
+        var expected = CSharpAnalyzerVerifier<MustNotAllocateAnalyzer>
+            .Diagnostic(Diagnostics.SourceBoundary)
+            .WithLocation(6, 21)
+            .WithArguments("Caller", "MustNotAllocateAttribute", "Callee");
+
+        await CSharpAnalyzerVerifier<MustNotAllocateAnalyzer>
+            .VerifyAnalyzerAsync(source, expected);
+    }
+}
